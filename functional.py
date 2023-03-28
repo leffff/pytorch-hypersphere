328d47cad1fbdd9553f5bf193d74edd151e6be8c
dromanov <dtdzhalaev@gmail.com>
2023-03-28 13:26:30 +0300

    ♻️ Added doc-strings and improved imports

--- functional.py
+++ functional.py
@@ -1,5 +1,4 @@
 import torch
-from torch import arccos, cos, sin
 
 
 def to_hypersphere(x: torch.Tensor) -> torch.Tensor:
@@ -14,17 +13,17 @@ def to_hypersphere(x: torch.Tensor) -> torch.Tensor:
     phis = []
     for i in range(input_dim - 2):
         norm_i = torch.norm(x[:, i:], dim=1)
-        phi_i = arccos(x[:, i] / norm_i).reshape(-1, 1)
+        phi_i = torch.arccos(x[:, i] / norm_i).reshape(-1, 1)
         phis.append(phi_i)
 
-    phi_last = arccos(x[:, input_dim - 2] / torch.norm(x[:, input_dim - 2 :], dim=1))
+    phi_last = torch.arccos(x[:, input_dim - 2] / torch.norm(x[:, input_dim - 2:], dim=1))
     neg_mask = x[:, input_dim - 1] < 0
     phi_last[neg_mask] = 2 * torch.pi - phi_last[neg_mask]
     phi_last = phi_last.reshape(-1, 1)
 
-    hyperspherical = torch.cat([r, torch.cat(phis, dim=1), phi_last], dim=1)
+    hyper_spherical = torch.cat([r, torch.cat(phis, dim=1), phi_last], dim=1)
 
-    return hyperspherical
+    return hyper_spherical
 
 
 def to_euclidean(x: torch.Tensor) -> torch.Tensor:
@@ -39,14 +38,14 @@ def to_euclidean(x: torch.Tensor) -> torch.Tensor:
     xs = []
 
     cur_base = r
-    x_0 = cur_base * cos(x[:, 1])
+    x_0 = cur_base * torch.cos(x[:, 1])
     xs.append(x_0.reshape(-1, 1))
     for i in range(2, input_dim):
-        cur_base = cur_base * sin(x[:, i - 1])
-        x_i = cur_base * cos(x[:, i])
+        cur_base = cur_base * torch.sin(x[:, i - 1])
+        x_i = cur_base * torch.cos(x[:, i])
         xs.append(x_i.reshape(-1, 1))
 
-    x_last = cur_base * sin(x[:, input_dim - 1])
+    x_last = cur_base * torch.sin(x[:, input_dim - 1])
     xs.append(x_last.reshape(-1, 1))
 
     euclidean = torch.cat(xs, dim=1)
--- rand.py
+++ rand.py
@@ -2,15 +2,33 @@ import torch
 
 
 def nsphere_randn_spherical(shape: tuple, stretch_coefficient: float = 1.0) -> torch.Tensor:
+    """
+    Generate a random tensor in N-spherical space with the given shape and stretch coefficient.
+
+    :param shape: The shape of the tensor to generate.
+    :param stretch_coefficient: The stretch coefficient to apply to the tensor.
+
+    :return: A tensor with the given shape and stretch coefficient in N-spherical space.
+    """
     x = torch.randn(shape)
-    x_spherical = x / torch.norm(x, dim=1) * stretch_coefficient
+    x_norm = torch.norm(x, dim=1, keepdim=True)
+    x_spherical = x / x_norm * stretch_coefficient
 
     return x_spherical
 
 
 def euclidean_randn_spherical(shape: tuple, stretch_coefficient: float = 1.0) -> torch.Tensor:
+    """
+    Generate a random tensor in Euclidean space with the given shape and stretch coefficient,
+    and convert it to hyper-spherical space.
+
+    :param shape: The shape of the tensor to generate.
+    :param stretch_coefficient: The stretch coefficient to apply to the tensor.
+
+    :return: A tensor with the given shape and stretch coefficient in hyper-spherical space.
+    """
     r = torch.randn(shape) * stretch_coefficient
-    x_spherical = torch.FloatTensor(*shape).uniform_(0.0, torch.pi)
-    x_spherical[:, 0] = r
+    theta = torch.FloatTensor(*shape).uniform_(0.0)
+    x_spherical = torch.stack([r, theta.cos(), theta.sin()], dim=-1)
 
     return x_spherical
